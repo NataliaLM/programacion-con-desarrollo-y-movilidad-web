a8206b3a2d66ca1e35545aaeb9e7c883250747c4
NataliaLM <natalia.arq.123@gmail.com>
2022-07-06 14:58:38 +0200

    06/07/2022

--- javascript/JavaScript_Inicial/Ejemplo6-EstructurasControl/js/prueba3.js
+++ javascript/JavaScript_Inicial/Ejemplo6-EstructurasControl/js/prueba3.js
@@ -2,6 +2,12 @@ function mensaje(){
     var miClave=document.getElementById('clave').value;
     /* var eMail=document.getElementById('email').value; Otra forma*/
     var eMail=email.value;
+
+    var miNombre=nombre.value;
+    var miDomicilio=domicilio.value;
+    var miComunidad=comunidad.value;
+    var miProvincia=provincia.value;
+    var miSexo=sexo.value;
     /**
      * Ejercicio:
      * Establecer la validación de clave e email, en el sentido de que si
@@ -9,11 +15,13 @@ function mensaje(){
      * clave y email, en caso correcto dar la bienvenida al usuario.
      */
     
-    if(miClave.length!=0 && eMail.length != 0){ /*Si está relleno*/
+    if(miClave.length!=0 && eMail.length != 0 && miNombre.length != 0 &&
+        miDomicilio.length != 0 && miComunidad.length != 0 && 
+        miProvincia.length != 0 && miSexo.length != 0){ /*Si está relleno*/
         document.getElementById('resultado').innerHTML=
-        "Clave correcta, bienvenido a: "  + eMail + " pulse ingresar. ";
+        "Clave correcta, bienvenido a: "  + miNombre + " pulse ingresar. ";
     } else {
-        swal("Son necesarios clave e email. ");
+        swal("Son necesarios rellenar todos los campos. ");
     }
 }
 function limpiar(){
---
+++ javascript/JavaScript_Inicial/Ejemplo6-EstructurasControl/js/script.js
@@ -0,0 +1,12 @@
+/**
+ * Cálculo de crédito, 
+ * carrete
+ * crt = crédito*rédito*tiempo
+ * Cálculo de un crédito, donde cuando el tiempo sea a 24 meses, el tipo
+ * de interés será del 5%, a 60 meses un 8% y a partir de 60 meses al
+ * 10%.
+ */
+
+function Calcular(){
+
+}
